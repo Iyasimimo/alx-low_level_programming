8bb6b70fc48dd9ec1a1b56acf89d4478abcb0042
iyasimimo <israeladedapo19@gmail.com>
2022-10-20 20:44:27 -0700

    task 3

--- 0x03-debugging/3-print_remaining_days.c
+++ 0x03-debugging/3-print_remaining_days.c
@@ -8,7 +8,7 @@
  * @year: year
  * Return: void
  */
-voie print_remaining_days(int month, int day, int year)
+void print_remaining_days(int month, int day, int year)
 {
 	if ((year % 400 == 0) || (year % 4 == 0 && year % 100 != 0))
 	{
